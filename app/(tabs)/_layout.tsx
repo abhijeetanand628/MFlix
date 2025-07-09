c3acd3454609e86695806ff49b90f2be91c57682
Abhijeet Anand <abhijeetanand628@gmail.com>
2025-07-10 02:20:17 +0530

    started

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,113 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import {Image, ImageBackground, Text, View } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { images } from '@/constants/images'
+import { icons } from '@/constants/icons'
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+const TabIcon = ({focused, icon, title}: any) => {
+    if(focused) {
+    return (
+        <ImageBackground 
+            source={images.highlight}
+            style={{flex:1, flexDirection: 'row', width: '100%', minWidth: 100, minHeight: 52, 
+            marginTop: 14, justifyContent: 'center', alignItems: 'center', 
+            borderRadius: 100, overflow: 'hidden'}}
+            >
+            <Image source={icon} 
+            tintColor='#151312' 
+            style={{width: 20, height: 20}}
+            /> 
+            <Text style={{color: 'black', fontSize: 16, fontWeight: '500', marginLeft: 6}}>{title}</Text>
+        </ImageBackground>
+        )
+    }
+    
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', marginTop: 14, borderRadius: 9999}}>
+            <Image source={icon}
+            tintColor="#A8B5DB"
+            style={{width: 20, height: 20}}
+            />
+        </View>
+    )
+}
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const _Layout = () => {
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
+        screenOptions={{
+            tabBarShowLabel: false,
+            tabBarItemStyle: {
+                width: '100%',
+                height: '100%',
+                justifyContent: 'center',
+                alignContent: 'center'
+            },
+            tabBarStyle :{
+                backgroundColor: '#0f0D23',
+                borderRadius: 50,
+                marginHorizontal: 15,
+                marginBottom: 22,
+                height: 50,
+                position: 'absolute',
+                overflow: 'hidden',
+                borderWidth: 1,
+                borderColor: '0f0d23'
+                 
+            }
+        }}
+    
+    >
+        <Tabs.Screen 
         name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+        options = {{title: 'Home',
+             headerShown: false,
+             tabBarIcon: ({ focused }) => (
+                <TabIcon 
+                focused={focused} 
+                icon={icons.home} title="Home"
+                />
+             )
+            }}
+        />
+        <Tabs.Screen 
+        name="search"
+        options = {{title: 'Search',
+             headerShown: false,
+              tabBarIcon: ({ focused }) => (
+                <TabIcon 
+                focused={focused} 
+                icon={icons.search} title="Search"
+                />
+             ) 
+            }}
+        />
+        <Tabs.Screen 
+        name="saved"
+        options = {{title: 'Saved',
+             headerShown: false,
+             tabBarIcon: ({ focused }) => (
+                <TabIcon 
+                focused={focused} 
+                icon={icons.save} title="Saved"
+                />
+             ) 
+            }}
+        />
+        <Tabs.Screen 
+        name="profile"
+        options = {{title: 'Profile',
+             headerShown: false,
+             tabBarIcon: ({ focused }) => (
+                <TabIcon 
+                focused={focused} 
+                icon={icons.person} title="Profile"
+                />
+             ) 
+            }}
+        />
     </Tabs>
-  );
+  )
 }
+
+export default _Layout
---
+++ components/SearcBar.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SearcBar = () => {
+  return (
+    <View>
+      <Text>SearcBar</Text>
+    </View>
+  )
+}
+
+export default SearcBar
